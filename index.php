434b48598b0a096fc4c1022c1916201cb5cebbe3
SUBBU485 <1605485@kiit.ac.in>
2018-09-23 01:30:36 +0530

    Fi k

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+    
+    @media only screen and (max-width:620px) {
+  /* For mobile phones: */
+  .menu, .main, .right {
+    width:100%;
+  }
+}
+    </style>
 <body>
 <br>
 <br>
 <center>
-  <img src="logo.jpg" width="30%" height="30%">
+  <img src="logo.jpg" width="30%" height="30%" alt="centered image">
 </center>
 <br><br>
     <center><p><font size="7" color=""><b>COMING SOON</b></font></p></center>

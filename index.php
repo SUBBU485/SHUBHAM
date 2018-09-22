2efa3076eadfb63dfacf044aec5f0a08456dd03f
SUBBU485 <1605485@kiit.ac.in>
2018-09-22 23:19:30 +0530

    Foooo kk

--- index.php
+++ index.php
@@ -19,6 +19,12 @@ img {vertical-align: middle;}
   max-width: 1000px;
   position: relative;
 }
+@media only screen and (max-width:500px) {
+  /* For mobile phones: */
+  .menu, .main, .right {
+    width: 100%;
+  }
+}
 
 /* The dots/bullets/indicators */
 .dot {

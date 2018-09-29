73572165db87b2e4d76a6c5071292b59175ac059
SUBBU485 <1605485@kiit.ac.in>
2018-09-29 19:19:41 +0530

    fgtui

--- index.php
+++ index.php
@@ -1,22 +1,470 @@
 <!DOCTYPE html>
-<html>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<style>
-    
-    @media only screen and (max-width:620px) {
-  /* For mobile phones: */
-  .menu, .main, .right {
-    width:100%;
-  }
-}
-    </style>
+<html lang="en">
+<head>
+<title>YOURART</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Soft Decor Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<link href="css/menu.css" rel="stylesheet" type="text/css" media="all" /><!-- Navbar-CSS -->	
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+<!--//fonts-->
+	<style>
+		video {
+    max-width: 100%;
+    height: auto;
+            }</style>
+</head>
 <body>
-<br>
-<br>
-<center>
-  <img src="logo.jpg" width="30%" height="30%" alt="centered image">
-</center>
-<br><br>
-    <center><p><font size="7" color=""><b>COMING SOON</b></font></p></center>
+<!-- Header -->
+    <h1><a href="index.html"><img src="logo.jpg" style="width:200px;height:75px;"></a></h1>
+<div class="banner-top">
+		<!--div class="logo">
+			<h1><a href="index.html"><img src="logo.jpg" width="10%" height="10%"</a></h1>
+		</div>
+		<!-- Navigation -->
+		<div class="container">
+			<div class="menu">
+					<div class="overlay-navigation">
+						<nav role="navigation">
+							<ul>
+								<li><a href="#"  class="scroll" data-content="The beginning">Home</a></li>
+								<li><a href="#about"  class="scroll" data-content="Curious?">About</a></li>
+								<li><a href="#team"  class="scroll" data-content="designers">Designers</a></li>
+								<li><a href="#gallery"  class="scroll" data-content="Only the finest">Gallery</a></li>
+                                <li><a href="#foot"  class="scroll" data-content="Contacts">Contacts</a></li>
+							</ul>
+						</nav>
+					</div>
+					<section class="main">
+						<div class="open-overlay"> <span class="bar-top"></span> <span class="bar-middle"></span> <span class="bar-bottom"></span> </div>
+					</section>
+				</div>
+		</div>
+		<!-- //Navigation -->
+		<!-- banner-slider -->
+	
+			<video width="1400" height="540" autoplay controls>
+  <source src="yourfinalvid11.mp4" type="video/mp4">
+  <source src="yourfinalvid11.ogg" type="video/ogg">
+</video>
+			<!-- Slider -->
+		<!--div class="slider">
+			<ul class="rslides" id="slider">
+				<li class="first-img">
+					
+				</li>
+			</ul>
+		</div>
+		<!-- //Slider -->
+		<!-- //banner-slider -->
+	<!-- //Header -->
+	
+	<!-- welcome section -->
+	<div class="welcome" id="about">
+		<div class="container">
+			  <div class="welcome_sec">
+				<div class="welcome_sec_about-head">
+					<h3><span></span>About us<span class="head"></span></h3>
+				</div>
+				<div class="welcome_sec_about-grids">
+				 <div class="col-md-6 welcome_info">
+					  <h3>Welcome</h3>
+					  
+					  <h4>Who are we?</h4>
+					  <p>YourArt is all about creative designs and good quality products at reasonable prices.It is a platform where designers can showcase their creative and unique designs.We make good quality products with the use of these designs and sell them at reasonable rates.</p>
+					 <p>We are here to provide you with our attractive designer notebooks which you'd love to buy.</p>
+				
+				 </div>
+				 <div class="col-md-6 welcome_pic">
+					 <!--img src="n1.jpg" class="img-responsive" alt=""/-->
+					 <h3>What are our goals?</h3>
+					 <p>We aim to collaborate with designers who have good creative skills and can make unique designs which people would love.We want to help them grow by showcasing their skills.</p>
+					 <p>We also want to reduce the price of notebooks.This would enable the youth to get good unique notebooks at reasonable prices.We have tried to utilize the unused pages and covers on notebooks to help companies promote their brands.</p>
+                     <p>We also want to aim youth based startups and companies as it would be beneficial for them to target their potential customers through this way of marketing</p>
+				 </div>
+				 <div class="clearfix"></div>
+			  </div>
+			  </div>
+		</div>
+	</div>
+	<!-- //welcome section -->
+	<!-- team -->
+	<div class="team banner-bottom " id="team">
+		<div class="container">
+			<div class="team-head">
+				<h3>Our Designers</h3>
+			</div>
+			<div class="banner_bottom_grids">
+				<div class="col-md-3 agile_team_grid">
+					<div class="team_grid">
+						<img src="asdr.jpg" alt=" " class="img-responsive" />
+						<div class="team_grid_pos">
+							<div class="text">
+								<ul class="agileits_social_list">
+									<li><a href="#" class="agile_facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="#" class="agile_twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a href="#" class="agile_dribble"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<h4>Shawan</h4>
+					<p>Designer</p>
+				</div>
+				<div class="col-md-3 agile_team_grid">
+					<div class="team_grid">
+						<img src="IMG_8918.jpg" alt=" " class="img-responsive" />
+						<div class="team_grid_pos">
+							<div class="text">
+								<ul class="agileits_social_list">
+									<li><a href="#" class="agile_facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="#" class="agile_twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a href="#" class="agile_dribble"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<h4>Kashish Dodai</h4>
+					<p>Designer</p>
+				</div>
+				<div class="col-md-3 agile_team_grid">
+					<div class="team_grid">
+						<img src="IMG-20171215-WA0014.jpg" alt=" " class="img-responsive" />
+						<div class="team_grid_pos">
+							<div class="text">
+								<ul class="agileits_social_list">
+									<li><a href="#" class="agile_facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="#" class="agile_twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a href="#" class="agile_dribble"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<h4>Neha Lal</h4>
+					<p>Designer</p>
+				</div>
+				<div class="col-md-3 agile_team_grid">
+					<div class="team_grid">
+						<img src="surabhifinall.jpg" alt=" " class="img-responsive" />
+						<div class="team_grid_pos">
+							<div class="text">
+								<ul class="agileits_social_list">
+									<li><a href="#" class="agile_facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="#" class="agile_twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a href="#" class="w3_agile_dribble"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<h4>Surabhi Ramrakhiani</h4>
+					<p>Designer</p>
+				</div>
+
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+<!-- //team -->
+<!-- gallery -->
+	<div class="gallery" id="gallery">
+	 <div class="heading">
+				<h3>Our Gallery</h3>
+			</div>
+	 <div class="grids gal-agileits">
+			<div id="jzBox" class="jzBox">
+				<div id="jzBoxNextBig"></div>
+				<div id="jzBoxPrevBig"></div>
+				<img src="#" id="jzBoxTargetImg" alt=""/>
+				<div id="jzBoxBottom">
+					<div id="jzBoxTitle"></div>
+					<div id="jzBoxMoreItems">
+						<div id="jzBoxCounter"></div>
+						<i class="arrow-left" id="jzBoxPrev"></i> 
+						<i class="arrow-right" id="jzBoxNext"></i> 
+					</div>
+					<i class="close" id="jzBoxClose"></i>
+				</div>
+			</div>
+			<div class="gallery-grids-row">
+				<div class="col-md-3 gallery-grid">
+					<div class="wpf-demo-4">  
+						<a href="jpeg(1).jpg" class="jzBoxLink item-hover" title="Education">  
+							<img src="jpeg(1).jpg" alt=" " class="img-responsive" />
+							<div class="view-caption">
+								<p>Full View</p>
+							</div> 
+						</a>    		
+					</div>
+				</div>
+				<div class="col-md-3 gallery-grid">
+					<div class="wpf-demo-4">  
+						<a href="IMG_1648.jpg" class="jzBoxLink item-hover" title="Education">  
+							<img src="IMG_1648.jpg" alt=" " class="img-responsive" />
+							<div class="view-caption">
+								<p>Full View</p>
+							</div> 
+						</a>    			
+					</div>
+				</div>
+				<div class="col-md-3 gallery-grid">
+					<div class="wpf-demo-4">  
+						<a href="football_300.jpg" class="jzBoxLink item-hover" title="Education">  
+							<img src="football_300.jpg" alt=" " class="img-responsive" />
+							<div class="view-caption">
+								<p>Full View</p>
+							</div> 
+						</a>    			
+					</div>
+				</div>
+				<div class="col-md-3 gallery-grid">
+					<div class="wpf-demo-4">  
+						<a href="IMG_164511.jpg" class="jzBoxLink item-hover" title="Education">  
+							<img src="IMG_164511.jpg" alt=" " class="img-responsive" />
+							<div class="view-caption">
+								<p>Full View</p>
+							</div> 
+						</a>    			
+					</div>
+				</div>
+				<!--div class="col-md-3 gallery-grid">
+					<div class="wpf-demo-4">  
+						<a href="jpeg(1).jpg" class="jzBoxLink item-hover" title="Education">  
+							<img src="jpeg(1).jpg" alt=" " class="img-responsive" />
+							<div class="view-caption">
+								<p>Full View</p>
+							</div> 
+						</a>    		
+					</div>
+				</div>
+				<!--div class="col-md-3 gallery-grid">
+					<div class="wpf-demo-4">  
+						<a href="football_300.jpg" class="jzBoxLink item-hover" title="Education">  
+							<img src="football_300.jpg" alt=" " class="img-responsive" />
+							<div class="view-caption">
+								<p>Full View</p>
+							</div> 
+						</a>    		
+					</div>
+				</div>
+				<!--div class="col-md-3 gallery-grid">
+					<div class="wpf-demo-4">  
+						<a href="IMG_164511.jpg" class="jzBoxLink item-hover" title="Education">  
+							<img src="IMG_164511.jpg" alt=" " class="img-responsive" />
+							<div class="view-caption">
+								<p>Full View</p>
+							</div> 
+						</a>    			
+					</div>
+				</div>
+				<!--div class="col-md-3 gallery-grid">
+					<div class="wpf-demo-4">  
+						<a href="IMG_1648.jpg" class="jzBoxLink item-hover" title="Education">  
+							<img src="IMG_1648.jpg" alt=" " class="img-responsive" />
+							<div class="view-caption">
+								<p>Full View</p>
+							</div> 
+						</a>    		
+					</div>
+				</div-->  
+				
+				<div class="clearfix"> </div>
+			</div>
+			</div>
+	</div>
+	<!-- //gallery --> 
+	
+	<div class="footer" id="foot">
+		<div class="container">
+			<div class="footer-row">
+				<div class="col-md-3 footer-grids footer-address">
+					<h3><a href="#"><span>YOUR ART</a></h3>
+				</div>
+				<div class="col-md-6 footer-grids subscribe">
+					<form action="#" method="post">
+						<input type="text" name="Email" placeholder="Your Email" required="">
+						<input type="submit" value="Subscribe">
+					</form>
+					<div class="clearfix"> </div> 
+				</div>
+				<div class="col-md-3 footer-grids footer-icons">
+					<div class="footer-social-grids">
+                            <ul>
+							<li><a href="https://www.facebook.com/official.yourart"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="https://www.instagram.com/yourart1"><i class="fa fa-instagram"></i></a></li>
+							<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+				
+				<div class="footer-grids">
+					<div class="col-md-4 footer-grid footer-grid1 ">
+						<div class="footer-logo">
+							<a href="#">HOW U CAN ORDER</a>
+							<p>1.Whatsapp us at 8637282534</p>
+							<p>2.Contact us at 8637282534</p>
+                            <P>3.Mail us at official.yourart@gmail.com</P>
+						</div>
+					</div>
+					<div class="col-md-4 footer-grid footer-grid2">
+						<h3>Navigation</h3>
+						<ul>
+							<li> <a href="#about" class="scroll">About Us</a> </li>
+							
+							<li> <a href="#gallery" class="scroll">Gallery</a> </li>
+							<li> <a href="#contact" class="scroll">Contact Us</a> </li>
+						</ul>
+					</div>
+					<div class="col-md-4 agile-footer-grid footer-grid3">
+						<h3>Contact Us</h3>
+					<ul>
+						<li><i class="glyphicon glyphicon-send"></i> Upper Telenga Bazar<span>,PuriGhat,Cuttack. </span></li>
+						<li><i class="glyphicon glyphicon-phone"></i> +91 <span> 8637282534 </span></li>
+						<li><i class="glyphicon glyphicon-envelope"></i> official.yourart@gmail.com</li>
+					</ul>
+
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+
+			</div>
+			<div class="footer-bottom">
+			<p>© 2018 YourArt. All rights reserved</a></p>
+
+			</div>
+		</div>
+	</div>
+	<!-- //footer --> 
+
+
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+
+<!--nav-script-->
+	<script>
+		$('.open-overlay').click(function() {
+		  var overlay_navigation = $('.overlay-navigation'),
+			nav_item_1 = $('nav li:nth-of-type(1)'),
+			nav_item_2 = $('nav li:nth-of-type(2)'),
+			nav_item_3 = $('nav li:nth-of-type(3)'),
+			nav_item_4 = $('nav li:nth-of-type(4)'),
+			nav_item_5 = $('nav li:nth-of-type(5)'),
+			top_bar = $('.bar-top'),
+			middle_bar = $('.bar-middle'),
+			bottom_bar = $('.bar-bottom');
+
+		  overlay_navigation.toggleClass('overlay-active');
+		  if (overlay_navigation.hasClass('overlay-active')) {
+
+			top_bar.removeClass('animate-out-top-bar').addClass('animate-top-bar');
+			middle_bar.removeClass('animate-out-middle-bar').addClass('animate-middle-bar');
+			bottom_bar.removeClass('animate-out-bottom-bar').addClass('animate-bottom-bar');
+			overlay_navigation.removeClass('overlay-slide-up').addClass('overlay-slide-down')
+			nav_item_1.removeClass('slide-in-nav-item-reverse').addClass('slide-in-nav-item');
+			nav_item_2.removeClass('slide-in-nav-item-delay-1-reverse').addClass('slide-in-nav-item-delay-1');
+			nav_item_3.removeClass('slide-in-nav-item-delay-2-reverse').addClass('slide-in-nav-item-delay-2');
+			nav_item_4.removeClass('slide-in-nav-item-delay-3-reverse').addClass('slide-in-nav-item-delay-3');
+			nav_item_5.removeClass('slide-in-nav-item-delay-4-reverse').addClass('slide-in-nav-item-delay-4');
+		  } else {
+			top_bar.removeClass('animate-top-bar').addClass('animate-out-top-bar');
+			middle_bar.removeClass('animate-middle-bar').addClass('animate-out-middle-bar');
+			bottom_bar.removeClass('animate-bottom-bar').addClass('animate-out-bottom-bar');
+			overlay_navigation.removeClass('overlay-slide-down').addClass('overlay-slide-up')
+			nav_item_1.removeClass('slide-in-nav-item').addClass('slide-in-nav-item-reverse');
+			nav_item_2.removeClass('slide-in-nav-item-delay-1').addClass('slide-in-nav-item-delay-1-reverse');
+			nav_item_3.removeClass('slide-in-nav-item-delay-2').addClass('slide-in-nav-item-delay-2-reverse');
+			nav_item_4.removeClass('slide-in-nav-item-delay-3').addClass('slide-in-nav-item-delay-3-reverse');
+			nav_item_5.removeClass('slide-in-nav-item-delay-4').addClass('slide-in-nav-item-delay-4-reverse');
+		  }
+		})
+	</script>
+	<!--nav-script-->
+	<script src="js/jzBox.js"></script>
+
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+<script src="js/responsiveslides.min.js"></script>
+	<script>
+		$(function () {
+			$("#slider").responsiveSlides({
+				auto: true,
+				nav: true,
+				speed: 800,
+				namespace: "callbacks",
+				pager: true,
+			});
+		});
+	</script>
+	<!-- flexSlider -->
+					<script defer src="js/jquery.flexslider.js"></script>
+					<script type="text/javascript">
+						$(window).load(function(){
+						  $('.flexslider').flexslider({
+							animation: "slide",
+							start: function(slider){
+							  $('body').removeClass('loading');
+							}
+						  });
+						});
+					</script>
+				<!-- //flexSlider -->
+<script src="js/SmoothScroll.min.js"></script>
+	<script type="text/javascript">
+		/* init Jarallax */
+		$('.jarallax').jarallax({
+			speed: 0.5,
+			imgWidth: 1366,
+			imgHeight: 768
+		})
+	</script>
+<script type="text/javascript">
+					jQuery(document).ready(function($) {
+						$(".scroll").click(function(event){		
+							event.preventDefault();
+							$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+						});
+					});
+				</script>
+
+<!-- smooth scrolling -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+		/*
+			var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+			};
+		*/								
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	<a href="#home" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!-- //smooth scrolling -->
+<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
 </body>
 </html>

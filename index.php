f9a3136155700514dfd39ec0a3e992348bc1c72c
SUBBU485 <1605485@kiit.ac.in>
2018-09-30 20:52:51 +0530

    fgtui

--- index.php
+++ index.php
@@ -115,7 +115,7 @@
   </div>
 </div-->
     
- <nav class="navbar navbar-expand-md navbar-light navbar-fixed-top">
+ <nav class="navbar navbar-expand-md navbar-light">
   <a class="navbar-brand" href="#"><img src="newlogo.png" style="width:200px;height:75px;margin-top:-20px;margin-left:-10px" ></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon" style="color:black"></span>

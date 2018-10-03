955afb8569cf66a0eeb1c98a875944c3004c49a7
SUBBU485 <1605485@kiit.ac.in>
2018-10-03 22:11:06 +0530

    last

--- index.php
+++ index.php
@@ -275,11 +275,11 @@
   </div>
   
   <!-- Left and right controls -->
-  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-    <span class="carousel-control-prev-icon" style="color:black"></span>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev" style="color:black">
+    <span class="carousel-control-prev-icon" ></span>
   </a>
-  <a class="carousel-control-next" href="#demo" data-slide="next">
-    <span class="carousel-control-next-icon" style="color:black"></span>
+  <a class="carousel-control-next" href="#demo" data-slide="next" style="color:black">
+    <span class="carousel-control-next-icon"></span>
   </a>	 
 </div>
 </div>

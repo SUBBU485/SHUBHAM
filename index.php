df4682f1034ac62df74ad9f18d0f88a4ef9fd690
SUBBU485 <1605485@kiit.ac.in>
2019-01-15 22:06:02 +0530

    latest

--- index.php
+++ index.php
@@ -97,7 +97,7 @@
 		<br>
 		<br>
   <img src="coverpage.jpg" alt="Nature" class="responsive" height="530">
-    <p>REVEALING NEW DESIGNS! AVAIL DISCOUNT BY EARLY GRAB</p>
+    <p><b><i>REVEALING NEW DESIGNS! AVAIL DISCOUNT BY EARLY GRAB</i></b></p>
 	<!-- welcome section -->
 	<div class="welcome" id="about">
 		<div class="container">
